50eb66b3b13a557e811bf4a07fb226717de3fe7b
Shubham Shah <shubhamshahrising@gmail.com>
2023-04-03 18:45:30 +0530

    feat: added markdown and custom suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Badge, Card, Container, ListGroup, Navbar } from "react-bootstrap";
+import { Badge, Container, ListGroup, Navbar } from "react-bootstrap";
 import {
   ReactiveBase,
   ReactiveList,
@@ -11,6 +11,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./App.module.css";
 
 import "./App.css";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 function Main() {
   return (
@@ -28,7 +29,7 @@ function Main() {
         userId: "jon",
       }}
     >
-      <Navbar bg="white" className="shadow" expand="lg">
+      <Navbar bg="white" className="shadow mb-5" expand="lg">
         <Container>
           <Navbar.Brand>Reactivesearch</Navbar.Brand>
           <span className={`text-white ${styles.headingTag}`}>KNN Search</span>
@@ -40,27 +41,69 @@ function Main() {
         dataField={[
           {
             field: "keywords",
-            weight: 10,
+            weight: 4,
           },
           {
-            field: "meta_description",
-            weight: 3,
-          },
-          {
-            field: "meta_title",
-            weight: 3,
-          },
-          {
-            field: "tokens",
-            weight: 1,
+            field: "heading",
+            weight: 2,
           },
         ]}
+        className="mx-5 mt-2"
         componentId={"search"}
         URLParams
         showClear
+        distinctField="meta_title.keyword"
         debounce={500}
         highlight={false}
+        render={({
+          data,
+          downshiftProps: {
+            isOpen,
+            getItemProps,
+            highlightedIndex,
+            selectedItem,
+          },
+        }) => {
+          return isOpen ? (
+            <div className={`${styles.suggestions}`}>
+              {data.map((item, index) => (
+                <div
+                  /* eslint-disable-next-line react/no-array-index-key */
+                  key={item._id + index}
+                  {...getItemProps({
+                    item,
+                    style: {
+                      backgroundColor:
+                        highlightedIndex === index
+                          ? "var(--bs-primary)"
+                          : "white",
+                      color:
+                        highlightedIndex === index
+                          ? "var(--bs-white)"
+                          : "var(--bs-black)",
+                      fontWeight: selectedItem === item ? "bold" : "normal",
+                      padding: "5px 15px",
+                    },
+                  })}
+                  className="listItem"
+                >
+                  <div className="fw-bold">{item.value.toUpperCase()}</div>
+                  <div>{`${item._source.meta_title} > ${item._source.heading}`}</div>
+                  <div>
+                    {item._source.keywords &&
+                      item._source.keywords.map((keyword) => (
+                        <Badge key="keyword" className="me-1">
+                          {keyword}
+                        </Badge>
+                      ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : null;
+        }}
       />
+
       <ReactiveList
         componentId="SearchResult"
         dataField="title"
@@ -86,11 +129,15 @@ function Main() {
                       <div>
                         {item.keywords &&
                           item.keywords.map((keyword) => (
-                            <Badge key="keyword">{keyword}</Badge>
+                            <Badge key="keyword" className="me-1">
+                              {keyword}
+                            </Badge>
                           ))}
                       </div>
-                      <div>{item.meta_description}</div>
-                      <div>{item.tokens}</div>
+                      <ReactMarkdown className="my-1">
+                        {item.meta_description}
+                      </ReactMarkdown>
+                      <div className="my-1">{item.heading}</div>
                     </ListGroup.Item>
                   ))}
                 </ListGroup>
